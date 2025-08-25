1f5a2ad8296c8c4bad4742331bb8bcff35dfe40f
Krazay17 <Joshmass7@gmail.com>
2025-08-25 17:13:39 -0400

    server heartbeat

--- server.js
+++ server.js
@@ -11,12 +11,37 @@ const io = new Server(server, {
     }
 });
 
+let players = {};
+
 io.on('connection', (socket) => {
+    players[socket.id] = { socket, lastPing: Date.now() };
     console.log('a user connected: ' + socket.id);
     socket.on('disconnect', () => {
         console.log('user disconnected: ' + socket.id);
+        delete players[socket.id];
+    });
+    socket.on('heartbeat', () => {
+        players[socket.id].lastPing = Date.now();
+    })
+
+    socket.on('playerReady', (data) => {
+        console.log('Player ready:', data);
+        const playerList = Object.keys(players).map(id => ({ id }));
+        socket.emit('initData', playerList);
     });
 });
 
+setInterval(() => {
+    Object.keys(players).forEach(key => {
+        const player = players[key];
+        if (!player) return;
+
+        if (Date.now() - player.lastPing > 5000) {
+            console.log(`Disconnecting inactive player: ${key}`);
+            player.socket.disconnect(true);
+        }
+    });
+}, 2500);
+
 server.listen(PORT);
-console.log('Sever is running on port 3000');
+console.log(`Server is running on port ${PORT}`);
